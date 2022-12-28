1466c784ac442afbc73d4c71f32b3b5ceccf874b
Sanduni Senarath <sandunisenarath@gmail.com>
2022-12-28 16:25:11 -0500

    throw error when number divded by 0 is inputted and disabled all buttons during that case except for "clear"

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ function multiply(array){
 
 function divide(array){
     return  array.reduce((prev, current) => {
-        return prev / current
+        if(current === 0){
+            return "ERROR"
+        } else {
+            return (prev / current)
+        }
+        
     })
 }
 
@@ -68,72 +73,157 @@ function operator(operator, array){
     }
 }
 
-console.log(operator("√",8))
-
 let displayEl = document.getElementById("display")
 let answerEl = document.getElementById("answer")
 let previousOperator = ""
 let numbers = []
 let operators = []
 let value1 = ""
+let value2 =""
 let answer = ""
+let operatorClicked = true
+let operatorValue =""
+let uniqueOperatorOn = true
 
-// store user inputted value
+
+// store numbers using button inputs
+
+function addOperator(sign){
+    operators.push(sign)  
+}
 
 document.querySelectorAll(".number").forEach(item => {
     item.addEventListener("click", event => {
-        value1 += event.target.innerHTML
-        displayEl.innerHTML += event.target.innerHTML
-        console.log(value1)
+        if(displayEl.innerHTML !== "ERROR"){
+            if(operatorClicked === true && operatorValue){
+                addOperator(operatorValue)
+                operatorClicked = false;
+            }
+            value1 += event.target.innerHTML
+            answerEl.innerHTML = value1
+        }
+
+    
     })
 })
 
+
+function storeUserNumber(){
+    displayEl.innerHTML += value1
+    value2 = value1
+    numbers.push(Number(value2))
+    value1 =""
+}
+
+function renderAnswer(){
+    answer = operator(operators.shift(),numbers)
+    if(answer === "ERROR"){
+        clear()
+        displayEl.innerHTML = "ERROR"
+    } else {
+        numbers = [answer]
+        displayEl.innerHTML = answer
+    }
+
+}
+
+// add functionality for all other operators
+
 document.querySelectorAll(".operator").forEach(item => {
     item.addEventListener("click", event => {
-        displayEl.innerHTML += event.target.innerHTML
-        numbers.push(Number(value1)) // push user input value to array
-        console.log(numbers)
-        value1 = "" // reset value for next user input
-        operators.push(event.target.innerHTML)
-        if(numbers.length >= 2) {
-            previousOperator = operators.shift()
-            answer = operator(previousOperator,numbers)
-            answerEl.innerHTML = answer
-            displayEl.innerHTML = `${answer}${operators[0]}`
-            numbers = [answer]
+        if(displayEl.innerHTML !== "ERROR"){
+            uniqueOperatorOn = false;
+            storeUserNumber()
+            if(operatorClicked && operatorValue){
+                operatorValue = event.target.innerHTML;
+                displayEl.innerHTML = displayEl.innerHTML.slice(0,-1)
+                displayEl.innerHTML += operatorValue
+            } else if(operatorClicked) {
+                operatorValue = event.target.innerHTML;
+                displayEl.innerHTML += operatorValue
+            } else {
+                renderAnswer()
+                answerEl.innerHTML = ""
+                operatorValue = event.target.innerHTML;
+                displayEl.innerHTML += operatorValue
+                operatorClicked = true;
+            }    
         }
         
     })
 })
 
+// add functionalilty for equals button
+
+document.querySelector("#equals-el").addEventListener("click", event => {
+    if(numbers.length > 0){
+        storeUserNumber()
+        renderAnswer()
+        answerEl.innerHTML = answer
+        operatorClicked = true;
+        operatorValue =""
+        uniqueOperatorOn = true
+    }
+})
+
+// add functionality for √ and ! operators
 document.querySelectorAll(".unique-operator").forEach(item => {
     item.addEventListener('click', event => {
-        numbers.push(Number(value1))
-        answer = operator(event.target.innerHTML,numbers)
-        answerEl.innerHTML = answer
-        displayEl.innerHTML = `${answer}`
-        numbers = []
-        value1 = answer
+    if(displayEl.innerHTML !== "ERROR"){
+        if(uniqueOperatorOn){
+            if(numbers = []){
+                numbers.push(Number(value1))
+            }
+            console.log(numbers)
+            answer = operator(event.target.innerHTML,answerEl.innerHTML)
+            answerEl.innerHTML = answer
+            displayEl.innerHTML = `${answer}` 
+            numbers = []
+            value1 = answer
+        }
+    }
+        
     })
 })
 
-document.querySelector("#equals-el").addEventListener("click", event => {
-        numbers.push(Number(value1))
-        value1 = ""
-        previousOperator = operators.shift()
-        answer = operator(previousOperator,numbers)
-        numbers = []
-        value1 = answer
-        answerEl.innerHTML = answer
-        displayEl.innerHTML = answer
-})
 
+
+
+// add functionality for negatve button
 document.querySelector(".negative").addEventListener("click", () => {
-    value1 = Number(value1)*(-1)
-    displayEl.innerHTML = value1
+    if(displayEl.innerHTML !== "ERROR"){
+        answerEl.innerText = answerEl.innerHTML*(-1)
+        value1 = answerEl.innerText
+        console.log(value1)
+    }
+    
 })
 
+// add functionality for clear button
+
+document.querySelector(".clear-btn").addEventListener('click', () => {
+    clear()
+})
+
+// add functionality for delete button
+
+document.querySelector(".delete-btn").addEventListener('click', () => {
+    if(answerEl.innerHTML.length > 0){
+        displayEl.innerHTML = displayEl.innerHTML.slice(0,-1)
+        answerEl.innerHTML = answerEl.innerHTML.slice(0,-1)
+        value1 = answerEl.innerHTML
+    }
+
+})
+
+function clear(){
+    numbers = []
+    operators = []
+    value1 = ""
+    answer = ""
+    displayEl.innerHTML =""
+    answerEl.innerHTML=""
+    operatorClicked = true
+}
 
 
-let count ="22.2"
-console.log(Number(count))
